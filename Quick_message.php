6775da64c56310391697dd9ccaf1fc93294a6963
arul-manureva <kaarulmurugan.new@gmail.com>
2022-08-15 08:44:48 +0530

    first commit

---
+++ Quick_message.php
@@ -0,0 +1,22 @@
+<?php 
+$ToEmail = 'jescansandlabs@gmail.com'; 
+$EmailSubject = 'Contact Details'; 
+$mailheader = "From: ".$_POST["email"]."\r\n"; 
+$mailheader .= "Reply-To: ".$_POST["email"]."\r\n"; 
+$mailheader .= "Content-type: text/html; charset=iso-8859-1\r\n";
+$MESSAGE_BODY = '<html><body>';
+$MESSAGE_BODY .= '<p style="font-family:verdana;font-size:13px;"> Contact Details</p>';
+$MESSAGE_BODY .= '<table border="1" style="border-color: #d8d8d8;font-family:verdana; font-size:12px;" cellpadding="10" cellspacing="0">';
+$MESSAGE_BODY .= "<tr><td><strong>Email Address</strong> </td><td>" . strip_tags($_POST['email']) . "</td></tr>";
+$MESSAGE_BODY .= "<tr style='background: #afcbe7;'><td><strong>Message</strong> </td><td>" . strip_tags($_POST['message']) . "</td></tr>";
+$MESSAGE_BODY .= "</table>";
+$MESSAGE_BODY .= "</body></html>";
+
+ if($robotest)
+	$error = "Your email sending failed.";
+	else 
+	{
+	mail($ToEmail, $EmailSubject, $MESSAGE_BODY, $mailheader); 
+	echo "<html><body valign='center' align='center'><center><p style='font-family:verdana;font-size:12px;'>Thank You for contacting JE SCANS AND LABS  and we will revert you soon through your given contact number. </p> <p style='font-family:verdana;font-size:12px;'><a href='contact_form.html'> Back to Page</a></p></center></body></html>";	
+	}
+?>
